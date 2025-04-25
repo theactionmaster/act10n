77266d7800bf52b54bf9ebaf23fcb98f0a479761
theactionmaster <mainframe4money@gmail.com>
2025-04-25 17:25:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ def check_password():
     def password_entered(): 
         """Checks whether a password entered by the user is correct and sets the access level.""" 
         password = st.session_state.get("password", "")  # Use .get() to avoid KeyError
-        if password == st.secrets["PASSWORD"] or password == st.secrets["OTHERPW"] or password == st.secrets["BASE4PW"]: 
+        if password == st.secrets["PASSWORD"] or password == st.secrets["OTHERPW"] or password == st.secrets["BASE4PW"] or password == st.secrets["MMPW"]: 
             st.session_state["password_correct"] = True 
             st.session_state["access_level"] = "Platinum" 
         elif password == st.secrets["BASE3PW"]: 
